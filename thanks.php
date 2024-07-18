f32f739588eef8f764c38f7898bc5bf6d54a2864
daichiara18 <daichi.arhr@gmail.com>
2024-07-18 12:21:11 +0900

    Delete thanks.php

--- thanks.php
+++
@@ -1,6 +0,0 @@
-<?php include(dirname(__FILE__) . '/header.php'); ?>
-    <div class="message">
-        <p class="thank-you-message">お問い合わせありがとうございました。</p>
-    </div>
-<?php include(dirname(__FILE__) . '/footer.php'); ?>
-
